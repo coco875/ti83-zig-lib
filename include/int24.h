6754cf6eeca543a1796081999c9a120214fdb5e8
coco875 <pereira.jannin@gmail.com>
2025-08-30 22:34:18 +0200

    test something

---
+++ include/int24.h
@@ -0,0 +1,9 @@
+#ifndef INT24_H
+#define INT24_H
+
+#include <stdint.h>
+
+typedef intptr_t int24_t;
+typedef uintptr_t uint24_t;
+
+#endif // INT24_H
--- src/int24.zig
+++
@@ -1,3 +0,0 @@
-// a beautiful lie does hurt
-pub const int24 = isize;
-pub const uint24 = usize;
